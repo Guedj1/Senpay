35ddb43ac2799b2bde0ef48322ce23f47ac42983
* <louisndiogoye@gmail.comgit config --global user.name Guedj1git config --global user.name TON_USERNAMEgit config --global user.email louisndiogoye@gmail.com>
2025-12-09 11:54:12 +0000

    Ajout site pro Senpay thème sombre/clair

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function afficherMessage() {
+const button = document.getElementById("themeToggle");
+
+button.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    localStorage.setItem(
+        "theme",
+        document.body.classList.contains("dark") ? "dark" : "light"
+    );
+});
+
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark");
+}function afficherMessage() {
     document.getElementById("message").innerText = "Merci d'avoir visité Senpay !";
 }
 
