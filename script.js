3e506705742329f1b6716bb2bd69de3bd61af048
* <louisndiogoye@gmail.comgit config --global user.name Guedj1git config --global user.name TON_USERNAMEgit config --global user.email louisndiogoye@gmail.com>
2025-12-09 13:35:28 +0000

    Réinitialisation fichiers site à la racine

--- script.js
+++ script.js
@@ -10,7 +10,55 @@ button.addEventListener("click", () => {
 
 if (localStorage.getItem("theme") === "dark") {
     document.body.classList.add("dark");
-}function afficherMessage() {
-    document.getElementById("message").innerText = "Merci d'avoir visité Senpay !";
+}
+// Menu mobile
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+
+menuBtn.addEventListener("click", () => {
+    menu.classList.toggle("show");
+});
+// ===== SLIDER =====
+
+let index = 0;
+const slides = document.querySelectorAll(".slide");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+function showSlide(i) {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[i].classList.add("active");
+    document.querySelector(".slides").style.transform = `translateX(-${i * 100}%)`;
 }
 
+// Boutons
+next.addEventListener("click", () => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+});
+
+prev.addEventListener("click", () => {
+    index = (index - 1 + slides.length) % slides.length;
+    showSlide(index);
+});
+
+// Défilement automatique toutes les 5 sec
+setInterval(() => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+}, 5000);
+// ==== ANIMATION SCROLL ==== //
+
+const elements = document.querySelectorAll('.fade-in');
+
+function revealOnScroll() {
+    elements.forEach(el => {
+        const top = el.getBoundingClientRect().top;
+        if (top < window.innerHeight - 100) {
+            el.classList.add('visible');
+        }
+    });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
